603b2df037cc3b7de04b4baec819cb2eac518767
Jan.exe.999 <janat20071217@gmail.com>
2024-11-22 14:22:26 +0600

    done

---
+++ script/index.js
@@ -0,0 +1,44 @@
+const form = document.querySelector("form");
+const input = document.querySelector("#search");
+const select = document.querySelector("#count");
+const output = document.querySelector("#output");
+
+const API = "https://api.giphy.com/v1/gifs/search?";
+const key = "api_key=wOAnCvgZUfGwPw9oNH7N54K0qQj93aYW";
+const limit = "&limit=";
+const query = "&q=";
+
+const searchGiphy = async (e) => {
+    e.preventDefault();
+    if (!search.value.trim().length) return;
+
+    const url = API + key + limit + select.value + query + search.value
+    const req = await fetch(url)
+    const res = await req.json()
+    renderGiphys(res.data)
+}
+
+form.addEventListener("submit", searchGiphy)
+
+const renderGiphys = (data) => {
+    output.innerHTML = ``
+
+    data.forEach((el) => {
+        console.log(el)
+        const card = document.createElement("div")
+        const giff = document.createElement("img")
+        giff.src = el.images.preview_webp.url
+        giff.alt = el.alt_text
+
+        const giffName = document.createElement("h2")
+        giffName.textContent =
+            el.title.length > 15 ? el.title.slice(0 > 15) + "..." : el.title
+        giffName.title = el.title
+
+
+        card.append(giff, giffName)
+        output.append(card)
+    })
+
+
+} 
